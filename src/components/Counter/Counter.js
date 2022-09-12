8c647033078498ce56319db1f7d3b60e207a4767
Cristian Neely <cristianneely@gmail.com>
2022-09-12 19:44:33 -0300

    se agrega que el counter se detenga cuando se acaba el stock

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,9 +2,16 @@ import {useState} from 'react';
 
 const Counter = () => {
     const [initialState, setInitialState] = useState(0);
+    const [stock, setStock] = useState(3);
+    
 
     const suma = () => {
+        if(stock > 0){
         setInitialState(initialState + 1);
+        setStock(stock - 1);
+    }
+    else 
+    setInitialState("Producto Agotado!");
     };
 
     return(
@@ -12,6 +19,7 @@ const Counter = () => {
             Counter
             <h3>{initialState}</h3>
             <button onClick={suma}>Suma uno</button>
+            <h4>Stock: {stock}</h4>
         </div>
     )
 }
